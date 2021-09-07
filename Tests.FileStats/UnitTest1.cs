8bdfcc0c422744f2c706896ba013e93f0b1c7027
James A Sutherland <j@sutherland.pw>
2021-09-07 09:15:04 -0500

    Add basic testing

---
+++ Tests.FileStats/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System.IO;
+using System;
+
+namespace Tests.FileStats
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            using var stdout = new StringWriter();
+            using var stderr = new StringWriter();
+            Console.SetOut(stdout);
+            Console.SetError(stderr);
+            fileStats.FileStats.Main(new[] { "--bad" });
+            Assert.True(stderr.ToString().Contains("Option 'bad' is unknown"));
+        }
+    }
+}
