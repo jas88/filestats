fdf3f8e4ee80d836f5790cc821405f5fb63a436f
James A Sutherland <>
2023-12-07 16:56:53 -0600

    .Net 8, update for NUnit 4

--- Tests.FileStats/UnitTest1.cs
+++ Tests.FileStats/UnitTest1.cs
@@ -2,19 +2,18 @@ using NUnit.Framework;
 using System.IO;
 using System;
 
-namespace Tests.FileStats
+namespace Tests.FileStats;
+
+public class Tests
 {
-    public class Tests
+    [Test]
+    public void Test1()
     {
-        [Test]
-        public void Test1()
-        {
-            using var stdout = new StringWriter();
-            using var stderr = new StringWriter();
-            Console.SetOut(stdout);
-            Console.SetError(stderr);
-            fileStats.FileStats.Main(new[] { "--bad" });
-            Assert.True(stderr.ToString().Contains("Option 'bad' is unknown"));
-        }
+        using var stdout = new StringWriter();
+        using var stderr = new StringWriter();
+        Console.SetOut(stdout);
+        Console.SetError(stderr);
+        fileStats.FileStats.Main(new[] { "--bad" });
+        Assert.That(stderr.ToString().Contains("Option 'bad' is unknown"));
     }
 }
